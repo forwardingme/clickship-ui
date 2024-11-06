3a61bd082b827b1acf3ae0953c5c4786b43f0af1
Shuting Tang <tangst816@gmail.com>
2024-11-06 15:02:04 -0500

    Update new layout

--- src/app/shipment-type/shipment-type.component.ts
+++ src/app/shipment-type/shipment-type.component.ts
@@ -15,7 +15,7 @@ import { IconsComponent } from '../components/icons.component';
   template: `
   <div class="container">
     <app-header title="Select Shipping Type"></app-header>
-    <div class="d-flex flex-wrap align-items-center justify-content-center flex-fill">
+    <!--<div class="d-flex flex-wrap align-items-center justify-content-center flex-fill">
       <div class="d-flex justify-content-center py-2 gap-5">
         <div class="selection p-4" title="Envolope" (click)="setShipmentType(ParcelType.ENVELOPE)">
           <img class="logo_envolope" src="assets/images/envelope.svg" alt="Envelope" />
@@ -27,7 +27,24 @@ import { IconsComponent } from '../components/icons.component';
         </div>
       </div>
       <app-icons></app-icons>
+    </div>-->
+    <div class="row d-flex justify-content-center">
+      <div class="selection p-4 col-sm-5 d-flex justify-content-center" title="Envolope" (click)="setShipmentType(ParcelType.ENVELOPE)">
+        <div>
+          <img class="logo_envolope big-img" src="assets/images/envelope.svg" alt="Envelope" />
+          <div class="button">ENVELOPE</div>
+        </div>
+      </div>
+      <div class="vertical-line"></div>
+      <div class="selection p-4 col-sm-5 d-flex justify-content-center align-items-center" title="package" (click)="setShipmentType(ParcelType.PACKAGE)">
+        <div >
+        <img class="logo_package big-img" src="assets/images/box.svg" alt="Package" />
+        <div class="button">PACKAGE</div>
+        </div>
+      </div>
+     
     </div>
+    <app-icons></app-icons>
     <app-footer></app-footer>
   </div>
   `,
