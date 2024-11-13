0df536039fd5c91a5e468973468412e2d958e56b
Shuting Tang <tangst816@gmail.com>
2024-11-13 16:09:52 -0500

    update payment layout
    change the layout details

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -23,7 +23,7 @@ import {
 		<div class="container">
 			<app-header [showProgress]="false"></app-header>
 			<!-- <div class="page-title">Accept & Pay</div> -->
-			<div class="content" *ngIf="!accepted">
+			<div class="content">
 				<div class="row d-flex text-center mt-3 m-auto justify-content-around">
 					<div class="left col-md-6 col-sm-12">
 						<div class="rate m-auto">
@@ -56,7 +56,7 @@ import {
 							<button
 								type="button"
 								class="btn btn-danger btn-lg button"
-								(click)="onSubmit()"
+								(click)="onSubmit()" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
 							>
 								Accept & Pay
 							</button>
@@ -68,6 +68,7 @@ import {
 								alt="Payment Methods"
 							/>
 						</div>
+            <hr>
 						<div class="secure fs-5">Secure your rate!</div>
 						<div class="note">
 							DHL prices can change every 10 minutes. Finish your
@@ -76,12 +77,14 @@ import {
 						<div class="time-lapse fs-2">09:53</div>
 					</div>
 					<div class="right col-md-6 col-sm-12">
+            <!-- this is the carousel for the reviews-->
             <div id="carouselExampleIndicators" class="carousel carousel-dark slide">
               <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
               </div>
+             
               <div class="carousel-inner">
                 <div class="carousel-item active">
                   <div class="star-group d-flex justify-content-around">
@@ -130,39 +133,57 @@ import {
                 </div>
               </div>
             </div>
+            <!-- the end of the carousel for the reviews-->
+
+            <!-- here need the number of reviews-->
             <div><h6>12 268+ Happy Clients</h6></div>
             <hr>
+
+            <!-- the end of the carousel for the reviews-->
             <div class="h3 d-flex justify-content-start align-items-center">
               <img src="assets/images/payment-lock.png" width="50" alt="payment-lock">
               &nbsp;&nbsp;Safe Transaction
-            
             </div>
             <div class="h3 d-flex justify-content-start align-items-center">
               <img src="assets/images/payment-shield.png" width="50" alt="payment-shield">
               &nbsp;&nbsp;100% Secure Shipment 
             </div>
+             <!-- the end of the carousel for the reviews-->
 					</div>
 				</div>
 			</div>
-			<div class="content text-center" *ngIf="accepted">
-				<h3 class="m-auto">
-					Please use the card machine <br />
-					located to your right
-				</h3>
-				<div class="font-12 p-5">
-					Kindly attach the first label on the shipment <br />and
-					retain the second label for your records
-				</div>
-				<div class="text-center col-12 p-3">
-					<button
-						type="submit"
-						class="btn btn-danger btn-lg"
-						(click)="createShipment()"
-					>
-						Complete
-					</button>
-				</div>
-			</div>
+      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+          <div class="modal-body">
+            <div class="content text-center" *ngIf="accepted">
+              <img src="assets/images/credit-card-icon.png" width="150" alt="credit card icon">
+                <h3 class="w-100 m-auto">
+                  Please use the payment machine <br />
+                  located to your right
+                </h3>
+                <div class="font-16 p-5">
+                  Kindly attach the first label on the shipment <br />and
+                  retain the second label for your records
+                </div>
+                <div class="text-center col-12 p-3">
+                  <button
+                    type="submit"
+                    class="btn btn-danger btn-lg"
+                    (click)="createShipment()"
+                  >
+                  Complete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+			
 		</div>
 	`,
 	styleUrls: ['./payment.component.scss'],
